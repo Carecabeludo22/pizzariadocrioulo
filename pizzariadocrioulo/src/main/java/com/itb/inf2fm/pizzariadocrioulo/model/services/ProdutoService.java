b9d3a9dbf73182a8b9d558c5309a64a27e0b70f6
Carecabeludo22 <rm99666@estudante.fieb.edu.br>
2026-06-18 15:29:57 +0000

    Camada model services em construção

---
+++ pizzariadocrioulo/src/main/java/com/itb/inf2fm/pizzariadocrioulo/model/services/ProdutoService.java
@@ -0,0 +1,14 @@
+package com.itb.inf2fm.pizzariadocrioulo.model.services;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import com.itb.inf2fm.pizzariadocrioulo.model.entity.Produto;
+
+public class ProdutoService {
+
+    // Lista temporária de produtos
+
+    private static List<Produto> PRODUTOS = new ArrayList<>();
+
+}
